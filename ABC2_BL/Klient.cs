5d472c963a0a20f2f7e1a27e41481b9f871c1349
PiotrPal <133272105+PiotrPal@users.noreply.github.com>
2024-02-24 00:00:14 +0100

    init_0

---
+++ ABC2_BL/Klient.cs
@@ -0,0 +1,33 @@
+﻿namespace ABC2_BL
+{
+    public class Klient
+    {
+        private string nazwisko;
+
+        public string Nazwisko
+        {
+            get 
+            { 
+                //dodatkowy kod
+                return nazwisko; 
+            }
+            set 
+            {
+                //dodakowy kod
+                nazwisko = value;
+            }
+        }
+        public string Imie { get; set; }
+
+        public string Email { get; set; }
+
+        public int KlientID { get; private set; }
+        public string NazwiskoImie 
+        {
+           get
+            {
+                return Nazwisko + "," + Imie;
+            }
+        }
+    }
+}
